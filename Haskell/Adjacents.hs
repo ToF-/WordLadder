d3c612336f35162997b1657c122a8246ef122217
ToF <cthibaut@octo.com>
2018-07-09 10:05:30 +0200

    restart

--- Haskell/Adjacents.hs
+++ Haskell/Adjacents.hs
@@ -2,10 +2,10 @@ module Adjacents
 where
 import Data.List as L
 import Data.Map as M
+import Data.Set as S
 
-type Node = (String,(Int, Maybe String))
-type Path = M.Map String (Int, Maybe String)
 type Graph = M.Map String [String]
+type Path  = [String]
 
 
 adjacent :: String -> String -> Bool
@@ -18,42 +18,12 @@ adjacents :: [String] -> Graph
 adjacents ws = M.fromList $ L.filter (\(_,as) -> not (L.null as)) 
                 $ L.map (\w -> (w, L.filter (adjacent w) ws)) ws
 
-path :: String -> Path -> [String]
-path t ps = case M.lookup t ps of
-    Nothing -> []
-    Just (0,Nothing)  -> [t]
-    Just (l,(Just n)) -> path n ps ++ [t]
 
+ladders :: Graph -> String -> [Path] -> Set String -> [Path]
+ladders g t ps vs = L.filter (elem t) $ ps >>= search 
+    where 
+    search (x:xs) = L.map (\n-> (n:x:xs)) $  L.filter (\n -> not (S.member n vs)) (neighbors x g)
 
-ladder :: [String] -> String -> String -> [String]
-ladder _ s t | s == t = []
-ladder _ s t | length s /= length t = []
-ladder ws s t | not ((s `elem` ws) && (t `elem` ws)) = []
-ladder _ "dog" "bug" = []
-ladder ws s t = path t (ladder' (adjacents ws) (M.fromList [(s,(0,Nothing))]) M.empty)
-
-ladder' :: Graph -> Path-> Path -> Path
-ladder' _ tv vs | M.null tv = vs
-ladder' g tv vs = ladder' g tv'' vs' 
-    where
-    ((w,(l,lw)), tv') = M.deleteFindMin tv
-    ns :: [String]
-    ns = case M.lookup w g of
+    neighbors x g = case M.lookup x g of
         Nothing -> []
-        Just xs -> xs 
-
-    tv'' :: Path
-    tv''= insertNodes tv' (L.filter (\n-> M.lookup n vs' == Nothing) ns)
-
-    vs' :: Path
-    vs' = M.insertWith minNode w (l,lw) vs
-
-    insertNodes :: Path -> [String] -> Path
-    insertNodes p ns = L.foldr insertNode p ns 
-
-    insertNode :: String -> Path -> Path
-    insertNode s p = M.insertWith minNode s (l+1,Just w) p
-    
-    minNode :: (Int, Maybe String)->(Int, Maybe String) -> (Int, Maybe String) 
-    minNode (x,a) (y,b) | x <= y = (x,a)
-                        | otherwise = (y,b)
+        Just ns -> ns
--- Haskell/Specs.hs
+++ Haskell/Specs.hs
@@ -2,6 +2,7 @@ import Test.Hspec
 import Adjacents
 import Data.List
 import Data.Map as M (toList,fromList)
+import Data.Set as S
 
 ws = sort $ words "do dog fog fig fit fat cat cog cot bug bog bag bat"
 sp = words "dog cog cot cat"
@@ -37,40 +38,11 @@ main = hspec $ do
                 ,("fit",["fat","fig"])
                 ,("fog",["bog","cog","dog","fig"])]
 
-    describe "path" $ do
-        it "given a list of edges, yields a path" $ do
-            path "cat" (fromList ([("cat",(3,Just "cot"))
-                                  ,("cot",(2,Just "cog"))
-                                  ,("cog",(1,Just "dog"))
-                                  ,("dog",(0,Nothing))]))
-             `shouldBe` ["dog","cog","cot","cat"]
+    describe "ladders" $ do
+        it "given a graph, a target, a list of path and a set of visited nodes, yields a list of paths" $ do
+            let g = adjacents ws
+                i = [["dog"]]
+            ladders g "cog" i S.empty `shouldBe` [["cog","dog"]]
 
-    describe "ladder" $ do
-        it "given two adjacent words, finds the ladder" $ do
-            ladder ws "dog" "fog" `shouldBe` ["dog","fog"]
-            ladder ws "cot" "cat" `shouldBe` ["cot","cat"]
 
-        it "given two equal words, finds nothing" $ do
-            ladder ws "dog" "dog" `shouldBe` []
 
-        it "given two distant words, finds nothing" $ do
-            ladder ws "dog" "bug" `shouldBe` []
-
-        it "given at least one non-existing word, finds nothing" $ do
-            ladder ws "dog" "foo" `shouldBe` []
-            ladder ws "bar" "cat" `shouldBe` []
-
-        it "given words of different size, finds nothing" $ do
-            ladder ws "do" "dog" `shouldBe` []
-
-        it "given words at a distance of 2, finds the ladder" $ do
-            ladder ws "dog" "fig" `shouldBe` ["dog","fog","fig"]
-
-        it "given words at a distance of 3, finds the ladder" $ do
-            ladder ws "dog" "fit" `shouldBe` ["dog","fog","fig","fit"]
-
-        it "always finds the shortest ladder" $ do
-            ladder ws "dog" "cat"  `shouldBe` ["dog","cog","cot","cat"]
-            ladder (ws\\["cot"]) "dog" "cat"  `shouldBe` ["dog","bog","bag","bat","cat"]
-            ladder (ws\\["cot","bag"]) "dog" "cat"  `shouldBe` ["dog","fog","fig","fit","fat","cat"]
-        
