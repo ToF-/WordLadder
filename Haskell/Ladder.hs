abe581366ed91374ee1ca468f3b66251a3cd1607
ToF <cthibaut@octo.com>
2018-07-22 20:25:40 +0200

    extract method

--- Haskell/Ladder.hs
+++ Haskell/Ladder.hs
@@ -2,6 +2,9 @@ module Ladder where
 
 ladder :: [String] -> String -> String -> [String]
 ladder _ s t | s == t = []
-ladder _ "DOG" "COG" = ["DOG","COG"]
-ladder _ "DOG" "BOG" = ["DOG","BOG"]
+ladder _ s t | s `adjacent` t = [s,t]
+    where
+    adjacent "DOG" "COG" = True
+    adjacent "DOG" "BOG" = True
+    adjacent _ _ = False
 ladder _ _ _ = ["DOG","COG","COT","CAT"]
