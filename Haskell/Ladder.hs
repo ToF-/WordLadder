e8d828a19bb4e6a0281846aa630d0a9716799eea
ToF <cthibaut@octo.com>
2018-07-22 20:21:39 +0200

    ladder with two adjacent words

--- Haskell/Ladder.hs
+++ Haskell/Ladder.hs
@@ -2,4 +2,6 @@ module Ladder where
 
 ladder :: [String] -> String -> String -> [String]
 ladder _ s t | s == t = []
+ladder _ "DOG" "COG" = ["DOG","COG"]
+ladder _ "DOG" "BOG" = ["DOG","BOG"]
 ladder _ _ _ = ["DOG","COG","COT","CAT"]
--- Haskell/Specs.hs
+++ Haskell/Specs.hs
@@ -9,6 +9,10 @@ main = hspec $ do
             it "gives the adjacent words between a source and a target" $ do
                 ladder_ "DOG" "CAT" `shouldBe` "DOG COG COT CAT" 
 
+            it "gives a 2 step ladder when the words are adjacent" $ do
+                ladder_ "DOG" "COG" `shouldBe` "DOG COG"
+                ladder_ "DOG" "BOG" `shouldBe` "DOG BOG"
+
             it "gives nothing if the words are identical" $ do
                 ladder_ "DOG" "DOG" `shouldBe` ""
 
