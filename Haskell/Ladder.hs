d019be9af814acc24b117fde5793a1f4cec222d7
ToF- <cthibaut@octo.com>
2018-07-26 16:25:41 +0200

    search breadth first

--- Haskell/Ladder.hs
+++ Haskell/Ladder.hs
@@ -16,5 +16,21 @@ insert (a,b) s | (lookup a s) == Nothing = s ++ [(a,b)]
                | otherwise      = s
 
 union :: Queue -> Queue -> Queue
-union s t = foldl (\set e -> insert e set) s t
+union = foldl (\set e -> insert e set) 
 
+minus :: Queue -> Queue -> Queue
+minus q r = [(w,p) | (w,p) <- q, lookup w r == Nothing]
+
+path :: Queue -> String -> String -> [String]
+path q s t | s == t = [s]
+           | otherwise = case lookup t q of
+                Just p -> path q s p ++ [t]
+
+search :: Graph -> Queue -> Queue -> Queue
+search g [] done = done
+search g (edge:edges) done = search g (edges++(adjacents edge `minus` edges `minus` done)) (edge:done)
+    where 
+    adjacents :: Edge -> Queue
+    adjacents (w,_) = case lookup w g of
+        Nothing -> []
+        Just as -> map (\a -> (a,w)) as
