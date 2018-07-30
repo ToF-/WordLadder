c218ea830e5ec434ecbc29461a7a2cb7c8ec3b2a
ToF <cthibaut@octo.com>
2018-07-30 18:24:58 +0200

    edge can change its from word key

--- Rust/word_ladder/src/lib.rs
+++ Rust/word_ladder/src/lib.rs
@@ -3,6 +3,12 @@ struct Edge<'a> {
     word: &'a str,
     from: Option<&'a str>
 }
+
+impl <'a> Edge<'a> {
+    fn set_from(&mut self, word:&'a str) {
+        self.from = Some(word)
+    }
+}
     
 
 #[cfg(test)]
@@ -10,11 +16,18 @@ mod edge_should {
     use super::*;
     
     #[test]
-    fn refer_to_a_word_and_a_from_node() {
+    fn refer_to_a_word_and_a_node() {
         let edge = Edge { word: "BAG", from:None };
 
         assert_eq!(edge.word,"BAG");
         assert_eq!(edge.from,None);
+    }
+    #[test]
+    fn change_its_from_key() {
+        let mut edge = Edge { word: "BAG", from:None };
+        edge.set_from("BUG");
+        assert_eq!(edge.from,Some("BUG"));
+
     }
 }
 
